b04a45921fa7db8f300b70c79bfa6f658ba2fb6b
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2024-03-26 12:40:22 +0530

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/jsx-pascal-case */
-
 import './App.css';
 import Header_top from './component/header_top/header_top';
+import Footer from './component/footer/footer';
 
 function App() {
   return (
     <div>
 
       <Header_top />
+      <Footer />
 
 </div>
   );
---
+++ src/component/footer/footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Box, Container } from "@mui/system";
+import { Grid } from "@mui/material";
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <Box>
+                <Container className='footer_topic'>
+                    <Grid container item >
+                        <Grid item xs={12} md={6} py={8}>
+                            <h1 >Branches</h1>
+                            <p> London to Europe, Sri Lanka, and Nigeria</p>
+                        </Grid>
+                        <Grid item xs={12} md={2} py={8} className='footer_mainlink' >
+                            <h4>Haqida</h4>
+                            <p><a href='/'>Book Online</a></p>
+                            <p><a href='/'>About Us</a></p>
+                            <p><a href='/'>Our Services</a></p>
+
+                        </Grid>
+                        <Grid item xs={12} md={2} py={8} className='footer_mainlink' >
+                            <h4>Links</h4>
+                            <p><a href='/'>Careers</a></p>
+                            <p><a href='/'>Help & Support</a></p>
+                            <p><a href='/'>Blog</a></p>
+
+                        </Grid>
+                        <Grid item xs={12} md={2} py={8} className='footer_mainlink' >
+                            <h4>Contact</h4>
+                            <p><a href='/'>Download Android App</a></p>
+                            <p><a href='/'>Download iOS App</a></p>
+                            <p><a href='/'>Terms and Condition</a></p>
+
+                        </Grid>
+                    </Grid>
+                    <h6>Developed By ITEYON</h6>
+                </Container>
+            </Box>
+        </div>
+    )
+}
+
+export default Footer
