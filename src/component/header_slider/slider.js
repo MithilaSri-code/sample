56e1f78c9b30312f90ae422db1aad91495bf1473
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2024-03-26 16:15:01 +0530

    fixing

--- src/component/header_slider/slider.js
+++ src/component/header_slider/slider.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import './slider.css'
 import localImage from '../../assest/feb-2024-all 1.png';
+import { Grid } from "@mui/material";
 
 const Slideshow = () => {
     return (
@@ -9,7 +10,10 @@ const Slideshow = () => {
             <Slide>
                 <img src={localImage} alt="main image" />
             </Slide>
+            
+
         </div>
+
     )
 }
 
