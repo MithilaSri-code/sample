7651e45ea90c0b5c67a79ae1c6c3652461a96220
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2024-03-26 15:46:39 +0530

    link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header_top from './component/header_top/header_top';
 import Footer from './component/footer/footer';
 import Main_image from './component/main_image/main_image';
 import Country_list from './component/country_list/country_list';
+import Invent_area from './component/invent_area/invent_area';
+import Reg_form from './component/reg_form/reg_form';
 
 function App() {
   return (
